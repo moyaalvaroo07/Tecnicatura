c4bf57e804c126dc8af2025506215c0c07f702b3
moyerita <moyaalvaroo07@gmail.com>
2023-11-06 17:46:04 -0300

    se agrega lecciones

---
+++ Java/semestre 1/src/clase6.java
@@ -0,0 +1,10 @@
+
+public class clase6 {
+   public static void main(String[] args) {
+        //Conversion de tipos primitivos
+        var edad = Integer.parseInt("20");
+        System.out.println("edad = " + (edad + 1));
+        var valorPI = Double.parseDouble("3.1416");
+        System.out.println("valorPI = " + valorPI);
+   } 
+}
